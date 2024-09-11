7a763ab68f5dd918b16e130d641f239edba1b69d
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-09-11 19:45:09 +0200

    Update inplayscenes.lang.php

--- inc/languages/deutsch_du/admin/inplayscenes.lang.php
+++ inc/languages/deutsch_du/admin/inplayscenes.lang.php
@@ -1,83 +1,98 @@
 <?php
 $l['inplayscenes_error_accountswitcher'] = "Das Plugin <a href=\"http://doylecc.altervista.org/bb/downloads.php?dlid=26&cat=2\" target=\"_blank\">\"Enhanced Account Switcher\"</a> muss installiert sein!";
 
-$l['inplayscenes_permission'] = 'Kann die Felder für den Inplaytracker verwalten?';
-$l['inplayscenes_nav'] = 'Eigene Inplaytrackerfelder';
+$l['inplayscenes_permission'] = "Kann die Felder für den Inplaytracker verwalten?";
+$l['inplayscenes_nav'] = "Eigene Inplaytrackerfelder";
 
-$l['inplayscenes_breadcrumb_main'] = 'Eigene Inplaytrackerfelder';
-$l['inplayscenes_breadcrumb_add'] = 'Neues Inplaytrackerfeld hinzufügen';
-$l['inplayscenes_breadcrumb_edit'] = 'Inplaytrackerfeld bearbeiten';
+$l['inplayscenes_breadcrumb_main'] = "Eigene Inplaytrackerfelder";
+$l['inplayscenes_breadcrumb_add'] = "Neues Inplaytrackerfeld hinzufügen";
+$l['inplayscenes_breadcrumb_edit'] = "Inplaytrackerfeld bearbeiten";
 
-$l['inplayscenes_tabs_overview'] = 'Eigene Inplaytrackerfelder';
-$l['inplayscenes_tabs_overview_desc'] = 'In diesem Bereich kannst du eigene Inplaytrackerfelder verwalten, erstellen und löschen.';
-$l['inplayscenes_tabs_add'] = 'Neues Inplaytrackerfeld hinzufügen';
-$l['inplayscenes_tabs_add_desc'] = 'Hier kannst du ein eigenes Profilfeld erstellen.';
-$l['inplayscenes_tabs_edit'] = 'Inplaytrackerfeld bearbeiten';
-$l['inplayscenes_tabs_edit_desc'] = 'Hier kannst du ein eigenes Inplaytrackerfelder bearbeiten.';
+$l['inplayscenes_tabs_overview'] = "Eigene Inplaytrackerfelder";
+$l['inplayscenes_tabs_overview_desc'] = "In diesem Bereich kannst du eigene Inplaytrackerfelder verwalten, erstellen und löschen.";
+$l['inplayscenes_tabs_add'] = "Neues Inplaytrackerfeld hinzufügen";
+$l['inplayscenes_tabs_add_desc'] = "Hier kannst du ein eigenes Profilfeld erstellen.";
+$l['inplayscenes_tabs_edit'] = "Inplaytrackerfeld bearbeiten";
+$l['inplayscenes_tabs_edit_desc'] = "Hier kannst du ein eigenes Inplaytrackerfelder bearbeiten.";
 
-$l['inplayscenes_overview_header'] = 'Eigene Inplaytrackerfelder';
-$l['inplayscenes_overview_container'] = 'Eigene Inplaytrackerfelder';
-$l['inplayscenes_overview_container_field'] = 'Inplaytrackerfelder';
-$l['inplayscenes_overview_container_require'] = 'Pflichtfeld?';
-$l['inplayscenes_overview_container_edit'] = 'Bearbeitbar?';
-$l['inplayscenes_overview_container_sort'] = 'Sortierung';
-$l['inplayscenes_overview_container_options'] = 'Steuerung';
-$l['inplayscenes_overview_yes'] = 'Ja';
-$l['inplayscenes_overview_no'] = 'Nein';
-$l['inplayscenes_overview_options_edit'] = 'Feld bearbeiten';
-$l['inplayscenes_overview_options_delete'] = 'Feld löschen';
-$l['inplayscenes_overview_options_delete_notice'] = 'Soll dieses Inplaytrackerfeld wirklich gelöscht werden?';
-$l['inplayscenes_overview_none'] = 'Bisher wurden keine eigenen Inplaytrackerfelder hinzugefügt.';
-$l['inplayscenes_overview_sort_button'] = 'Inplaytrackerfelder-Sortierung speichern';
-$l['inplayscenes_overview_sort_flash'] = 'Die Anzeigereihenfolge der Inplaytrackerfelder wurde erfolgreich aktualisiert.';
+$l['inplayscenes_overview_header'] = "Eigene Inplaytrackerfelder";
+$l['inplayscenes_overview_container'] = "Eigene Inplaytrackerfelder";
+$l['inplayscenes_overview_container_field'] = "Inplaytrackerfelder";
+$l['inplayscenes_overview_container_require'] = "Pflichtfeld?";
+$l['inplayscenes_overview_container_edit'] = "Bearbeitbar?";
+$l['inplayscenes_overview_container_sort'] = "Sortierung";
+$l['inplayscenes_overview_container_options'] = "Steuerung";
+$l['inplayscenes_overview_yes'] = "Ja";
+$l['inplayscenes_overview_no'] = "Nein";
+$l['inplayscenes_overview_options_edit'] = "Feld bearbeiten";
+$l['inplayscenes_overview_options_delete'] = "Feld löschen";
+$l['inplayscenes_overview_options_delete_notice'] = "Soll dieses Inplaytrackerfeld wirklich gelöscht werden?";
+$l['inplayscenes_overview_none'] = "Bisher wurden keine eigenen Inplaytrackerfelder hinzugefügt.";
+$l['inplayscenes_overview_sort_button'] = "Inplaytrackerfelder-Sortierung speichern";
+$l['inplayscenes_overview_sort_flash'] = "Die Anzeigereihenfolge der Inplaytrackerfelder wurde erfolgreich aktualisiert.";
 
-$l['inplayscenes_container_identification'] = 'Identifikator <em>*</em>';
-$l['inplayscenes_container_identification_desc'] = 'Dieser Identifikator dient als Orientierung in der Datenbank und für die Variabel-Bezeichnung im Template. Es sind keine Sonderzeichen oder Leerzeichen erlaubt (maschinenlesbar). Der Name muss für jedes Feld unterschiedlich sein.';
-$l['inplayscenes_container_title'] = 'Titel <em>*</em>';
-$l['inplayscenes_container_description'] = 'Kurzbeschreibung<em>*</em>';
-$l['inplayscenes_container_type'] = 'Feldtyp <em>*</em>';
-$l['inplayscenes_container_type_desc'] = 'Dies ist der Feldtyp, der gezeigt wird.';
-$l['inplayscenes_container_selectoptions'] = 'Auswählbare Optionen?<em>*</em>';
-$l['inplayscenes_container_selectoptions_desc'] = 'Gib jede Option in eine neue Zeile ein. Diese Einstellung trifft nur bei Auswahlboxen, Checkboxen und Radiobuttons zu.';
-$l['inplayscenes_container_disporder'] = 'Sortierung <em>*</em>';
-$l['inplayscenes_container_disporder_desc'] = 'Dies ist die Anzeigereihenfolge, abhängig von anderen Inplaytrackerfeldern. Die Zahl sollte für jedes Feld unterschiedlich sein.';
-$l['inplayscenes_container_require'] = 'Benötigt? <em>*</em>';
-$l['inplayscenes_container_require_desc'] = 'Muss dieses Feld bei einer Neueröffnung und bei der Bearbeitung von einer Szene ausgefüllt werden?';
-$l['inplayscenes_container_edit'] = 'Bearbeitbar? <em>*</em>';
-$l['inplayscenes_container_edit_desc'] = 'Kann das Feld im nachhinein noch bearbeitet werden?';
-$l['inplayscenes_container_parseroptions'] = 'MyCode-Optionen';
+$l['inplayscenes_container_identification'] = "Identifikator <em>*</em>";
+$l['inplayscenes_container_identification_desc'] = "Dieser Identifikator dient als Orientierung in der Datenbank und für die Variabel-Bezeichnung im Template. Es sind keine Sonderzeichen oder Leerzeichen erlaubt (maschinenlesbar). Der Name muss für jedes Feld unterschiedlich sein.";
+$l['inplayscenes_container_title'] = "Titel <em>*</em>";
+$l['inplayscenes_container_description'] = "Kurzbeschreibung<em>*</em>";
+$l['inplayscenes_container_type'] = "Feldtyp <em>*</em>";
+$l['inplayscenes_container_type_desc'] = "Dies ist der Feldtyp, der gezeigt wird.";
+$l['inplayscenes_container_selectoptions'] = "Auswählbare Optionen?<em>*</em>";
+$l['inplayscenes_container_selectoptions_desc'] = "Gib jede Option in eine neue Zeile ein. Diese Einstellung trifft nur bei Auswahlboxen, Checkboxen und Radiobuttons zu.";
+$l['inplayscenes_container_disporder'] = "Sortierung <em>*</em>";
+$l['inplayscenes_container_disporder_desc'] = "Dies ist die Anzeigereihenfolge, abhängig von anderen Inplaytrackerfeldern. Die Zahl sollte für jedes Feld unterschiedlich sein.";
+$l['inplayscenes_container_require'] = "Benötigt? <em>*</em>";
+$l['inplayscenes_container_require_desc'] = "Muss dieses Feld bei einer Neueröffnung und bei der Bearbeitung von einer Szene ausgefüllt werden?";
+$l['inplayscenes_container_edit'] = "Bearbeitbar? <em>*</em>";
+$l['inplayscenes_container_edit_desc'] = "Kann das Feld im nachhinein noch bearbeitet werden?";
+$l['inplayscenes_container_parseroptions'] = "MyCode-Optionen";
 
-$l['inplayscenes_type_text'] = 'Textbox';
-$l['inplayscenes_type_textarea'] = 'Textarea';
-$l['inplayscenes_type_select'] = 'Auswahlbox';
-$l['inplayscenes_type_multiselect'] = 'Auswahlbox mit mehreren Optionen';
-$l['inplayscenes_type_radio'] = 'Radiobuttons';
-$l['inplayscenes_type_checkbox'] = 'Checkboxen';
-$l['inplayscenes_type_date'] = 'Datum';
-$l['inplayscenes_type_url'] = 'URL';
+$l['inplayscenes_type_text'] = "Textbox";
+$l['inplayscenes_type_textarea'] = "Textarea";
+$l['inplayscenes_type_select'] = "Auswahlbox";
+$l['inplayscenes_type_multiselect'] = "Auswahlbox mit mehreren Optionen";
+$l['inplayscenes_type_radio'] = "Radiobuttons";
+$l['inplayscenes_type_checkbox'] = "Checkboxen";
+$l['inplayscenes_type_date'] = "Datum";
+$l['inplayscenes_type_url'] = "URL";
 
-$l['inplayscenes_container_parse_allowhtml'] = 'HTML erlauben';
-$l['inplayscenes_container_parse_allowmycode'] = 'MyCode erlauben';
-$l['inplayscenes_container_parse_allowimgcode'] = '[img]-Tags erlauben';
-$l['inplayscenes_container_parse_allowvideocode'] = '[video]-Tags erlauben';
+$l['inplayscenes_container_parse_allowhtml'] = "HTML erlauben";
+$l['inplayscenes_container_parse_allowmycode'] = "MyCode erlauben";
+$l['inplayscenes_container_parse_allowimgcode'] = "[img]-Tags erlauben";
+$l['inplayscenes_container_parse_allowvideocode'] = "[video]-Tags erlauben";
 
-$l['inplayscenes_error_identification'] = 'Du hast keinen Identifikator für dieses Inplaytrackerfeld angegeben.';
-$l['inplayscenes_error_title'] = 'Du hast keinen Titel für dieses Inplaytrackerfeld angegeben.';
-$l['inplayscenes_error_description'] = 'Du hast keine Beschreibung für dieses Inplaytrackerfeld angegeben.';
-$l['inplayscenes_error_selectoptions'] = 'Du hast keine auswählbare Optionen für dieses Inplaytrackerfeld angegeben.';
-$l['inplayscenes_error_invalid'] = 'Ungültige Option';
+$l['inplayscenes_error_identification'] = "Du hast keinen Identifikator für dieses Inplaytrackerfeld angegeben.";
+$l['inplayscenes_error_title'] = "Du hast keinen Titel für dieses Inplaytrackerfeld angegeben.";
+$l['inplayscenes_error_description'] = "Du hast keine Beschreibung für dieses Inplaytrackerfeld angegeben.";
+$l['inplayscenes_error_selectoptions'] = "Du hast keine auswählbare Optionen für dieses Inplaytrackerfeld angegeben.";
+$l['inplayscenes_error_invalid'] = "Ungültige Option";
 $l['inplayscenes_error_sort'] = "Beim Feststellen der Inplaytrackerfelder-Sortierung ist ein Fehler aufgetreten. Bitte aktualisiere die Seite und probiere es erneut.";
 
+$l['inplayscenes_add_header'] = "Neues Inplaytrackerfeld hinzufügen";
+$l['inplayscenes_add_container'] = "Neues Inplaytrackerfeld hinzufügen";
+$l['inplayscenes_add_button'] = "Inplaytrackerfeld speichern";
+$l['inplayscenes_add_flash'] = "Das Inplaytrackerfeld wurde erfolgreich erstellt.";
 
-$l['inplayscenes_add_header'] = 'Neues Inplaytrackerfeld hinzufügen';
-$l['inplayscenes_add_container'] = 'Neues Inplaytrackerfeld hinzufügen';
-$l['inplayscenes_add_button'] = 'Inplaytrackerfeld speichern';
-$l['inplayscenes_add_flash'] = 'Das Inplaytrackerfeld wurde erfolgreich erstellt.';
+$l['inplayscenes_edit_header'] = "Inplaytrackerfeld bearbeiten";
+$l['inplayscenes_edit_container'] = "Inplaytrackerfeld: {1}";
+$l['inplayscenes_edit_button'] = "Inplaytrackerfeld speichern";
+$l['inplayscenes_edit_flash'] = "Das Inplaytrackerfeld wurde erfolgreich gespeichert.";
 
-$l['inplayscenes_edit_header'] = 'Inplaytrackerfeld bearbeiten';
-$l['inplayscenes_edit_container'] = 'Inplaytrackerfeld: {1}';
-$l['inplayscenes_edit_button'] = 'Inplaytrackerfeld speichern';
-$l['inplayscenes_edit_flash'] = 'Das Inplaytrackerfeld wurde erfolgreich gespeichert.';
+$l['inplayscenes_delete_flash'] = "Das Inplaytrackerfeld wurde erfolgreich gelöscht.";
 
-$l['inplayscenes_delete_flash'] = 'Das Inplaytrackerfeld wurde erfolgreich gelöscht.';
+$l['inplayscenes_updates_nav'] = "Inplayszenen übertragen";
+$l['inplayscenes_updates_page'] = "Inplayszenen übertragen";
 
+$l['inplayscenes_updates_trackersystem'] = "Altes Inplayszenensystem wählen";
+$l['inplayscenes_updates_trackersystem_jule2'] = "Inplaytracker 2.0 von sparks fly";
+$l['inplayscenes_updates_trackersystem_jule3'] = "Inplaytracker 3.0 von sparks fly";
+$l['inplayscenes_updates_trackersystem_katja'] = "Szenentracker von risuena";
+
+$l['inplayscenes_updates_select'] = "Trackersystem";
+$l['inplayscenes_updates_select_desc'] = "Welches Tracker-Plugin hast du vorher benutzt? Bitte beachtet, das von einem unveränderten Plugin ausgegangen wird.";
+$l['inplayscenes_updates_button'] = "Daten übertragen";
+$l['inplayscenes_updates_none'] = "Die Inplayszenen wurden schon übertragen. Du kannst das alte Trackersystem deinstallieren.";
+
+$l['inplayscenes_updates_error'] = "Bitte wähle ein Trackersystem aus!";
+$l['inplayscenes_updates_update_flash'] = "Alle Szenen wurden erfolgreich übertragen. Du kannst nun das alte Trackersystem entfernen.";
+$l['inplayscenes_updates_error_flash'] = "Fehler beim Übertragen der Szenen. Bitte versuche es erneut";
