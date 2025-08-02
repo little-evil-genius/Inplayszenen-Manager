1a77df34f2bc37d016eef9cade78e905ee93d1ec
Lara <75126217+little-evil-genius@users.noreply.github.com>
2025-08-02 14:51:18 +0200

    Update inplayscenes.php

--- inc/plugins/inplayscenes.php
+++ inc/plugins/inplayscenes.php
@@ -1648,8 +1648,8 @@ function inplayscenes_admin_update_plugin(&$table) {
                 $table_status = $db->fetch_array($query);
                 $actual_collation = strtolower($table_status['Collation'] ?? '');
 
-                $actual_collation = str_replace(['utf8mb3', 'utf8mb4'], 'utf8', $actual_collation);
-                $expected_collation = str_replace(['utf8mb3', 'utf8mb4'], 'utf8', $collation);
+                $actual_collation = str_replace('utf8mb3', 'utf8', $actual_collation);
+                $expected_collation = str_replace('utf8mb3', 'utf8', $collation);
 
                 if (!empty($collation) && $actual_collation !== $expected_collation) {
                     $db->query("ALTER TABLE {$table} CONVERT TO CHARACTER SET {$charset} COLLATE {$collation}");
@@ -8802,8 +8802,8 @@ function inplayscenes_is_updated(){
         $result = $db->fetch_array($query);
         $actual_collation = strtolower($result['TABLE_COLLATION'] ?? '');
         
-        $actual_collation = str_replace(['utf8mb3', 'utf8mb4'], 'utf8', $actual_collation);
-        $expected_collation = str_replace(['utf8mb3', 'utf8mb4'], 'utf8', $collation);
+        $actual_collation = str_replace('utf8mb3', 'utf8', $actual_collation);
+        $expected_collation = str_replace('utf8mb3', 'utf8', $collation);
 
         if ($actual_collation !== $expected_collation) {
             return false;
@@ -8812,3 +8812,4 @@ function inplayscenes_is_updated(){
 
     return true;
 }
+
